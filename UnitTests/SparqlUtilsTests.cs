b875db429e95715614f6c2e221075cd347b9d71f
PHParis <pierrehenri.paris@gmail.com>
2019-11-14 16:17:32 +0100

    up

--- UnitTests/SparqlUtilsTests.cs
+++ UnitTests/SparqlUtilsTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using OntoSemStatsWeb.Utils;
 using OntoSemStatsWeb.Utils.Vocabularies;
 using VDS.RDF;
+using VDS.RDF.Parsing;
 using VDS.RDF.Query;
 using Xunit;
 
@@ -12,6 +13,57 @@ namespace UnitTests
 {
     public class UnitTest1
     {
+        // TODO: penser à tester si deux Inodes identiques appartenant à deux graphes différents sont considérés comme les mêmes ????
+        // TODO: y a t il un moyen plus simple de naviguer entre les propriétés et de charger les données manquantes éventuellement ??? Voir exemple de foaf ci-dessous
+
+        [Fact]
+        public async Task LoadFromUri()
+        {
+            // Télécharger le graphe de Paris
+            var g = new Graph();
+            try
+            {
+                g.LoadFromUri(new Uri("http://dbpedia.org/resource/Paris"), new NTriplesParser());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(g.Triples.Count);
+            Console.WriteLine(g.Triples.Select(x => x.Predicate).Distinct().Count());
+            foreach (var p in g.Triples.Select(x => x.Predicate).Distinct().OrderBy(x => x.ToString()))
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine($"Paris est sujet de certains triplets : {g.GetTriplesWithSubject(new Uri("http://dbpedia.org/resource/Paris")).Any()}");
+            Console.WriteLine($"Paris est objet de certains triplets : {g.GetTriplesWithObject(new Uri("http://dbpedia.org/resource/Paris")).Any()}");
+
+            // foaf name n'étant pas défini dans DBpedia comme ceci le prouve
+            var foafName = "http://xmlns.com/foaf/0.1/name";
+            var queryString = new SparqlParameterizedString();
+            queryString.CommandText = @"
+                SELECT * WHERE 
+                { 
+                    @property ?p ?o 
+                }";
+            queryString.SetUri("property", new Uri(foafName));
+
+            var results = await SparqlUtils.Select(queryString.ToString());
+            Console.WriteLine(results.IsEmpty);
+
+            // il faut donc charger la définition de foaf name depuis son URI
+            var g2 = new Graph();
+            try
+            {
+                g2.LoadFromUri(new Uri(foafName)); // ne marche pas avec RdfXmlParser
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(g2.Triples.Count);
+        }
+
         [Fact]
         public async Task FunctionalStatsOnDbpedia()
         {
