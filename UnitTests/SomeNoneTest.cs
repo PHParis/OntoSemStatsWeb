9ebb2702b77c9dfea8107c44ae5de9b79e140bcc
PHParis <pierrehenri.paris@gmail.com>
2019-11-21 16:26:08 +0100

    up

--- UnitTests/SomeNoneTest.cs
+++ UnitTests/SomeNoneTest.cs
@@ -3,18 +3,20 @@ using static LanguageExt.Prelude;
 using Xunit;
 using OntoSemStatsLib.ProcessResult;
 using VDS.RDF.Ontology;
+using System;
 
 namespace UnitTests
 {
     public class UnitTest2
     {
+        [Obsolete]
         [Fact]
         public void TestName()
         {
             var t1 = new Trpl<string>("a", "b", "c");
             Option<BasicStat> basicStat = t1.p switch
             {
-                OntologyHelper.PropertyDomain => new BasicStat(),
+                OntologyHelper.PropertyDomain => new BasicStat(t1),
                 _ => None
             };
             Assert.True(basicStat.IsNone);
@@ -22,7 +24,7 @@ namespace UnitTests
             var t2 = new Trpl<string>("a", OntologyHelper.PropertyDomain, "c");
             Option<BasicStat> basicStat2 = t2.p switch
             {
-                OntologyHelper.PropertyDomain => new BasicStat(),
+                OntologyHelper.PropertyDomain => new BasicStat(t2),
                 _ => None
             };
             Assert.True(basicStat2.IsSome);
@@ -30,7 +32,7 @@ namespace UnitTests
             var t3 = new Trpl<string>("a", OntologyHelper.PropertyDomain, "c");
             var basicStat3 = t3.p switch
             {
-                OntologyHelper.PropertyDomain => Some(new BasicStat()),
+                OntologyHelper.PropertyDomain => Some(new BasicStat(t3)),
                 _ => None
             };
             Assert.True(basicStat3.IsSome);
